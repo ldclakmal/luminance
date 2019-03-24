048e5993fcc5145e3ad57a9917a2f6892e0b319f
Chanaka Lakmal <ldclakmal@gmail.com>
2019-03-24 12:17:02 +0530

    Remove unused imports and refactor

--- index.php
+++ index.php
@@ -12,40 +12,20 @@
         </script>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-        <meta name="description" content="The Official Web Site of LUMINANCE | Photography">
-        <meta name="keywords" content="luminance, luminance.lk, photography, weddings" />
-        <title>LUMINANCE&reg; | Official</title>
-<!--        <link rel="shortcut icon" href="images/favicon.png?v=2">-->
-
-        <!-- Bootstrap -->
-        <link href="css/bootstrap-light.min.css" rel="stylesheet">
-        <link href="css/bootstrap-theme-light.min.css" rel="stylesheet">
-
-        <!-- Custom CSS -->
-        <link href="css/style-light.css" rel="stylesheet">
-
-        <!-- Superslides CSS -->
-        <link href="css/superslides.css" rel="stylesheet">
-
-        <!-- Custom CSS -->
-        <link href="css/custom.css" rel="stylesheet">
-
-        <!-- Google Fonts -->
-        <link href="http://fonts.googleapis.com/css?family=PT+Sans:400,700|Oranienbaum|PT+Serif:400italic" rel="stylesheet">
-
-        <!-- Font Awesome Icons -->
-        <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.2/css/font-awesome.css" rel="stylesheet">
-
+        <meta name="description" content="The Official Web Site of LUMINANCE.LK | Weddings | Photography | Videography">
+        <meta name="keywords" content="luminance, luminance.lk, luminance-weddings, weddings, photography, videography" />
+        <title>Luminance&reg;</title>
+        <link rel="shortcut icon" href="images/favicon.png?v=2">
+        <link rel="stylesheet" href="css/bootstrap-light.min.css" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/style-light.css" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" crossorigin="anonymous">
     </head>
     <body data-spy="scroll" data-target=".navbar-collapse" style="overflow-y: hidden;">
-
-    <!-- ========== BANNER START ========== -->
-
         <div id="banner">
             <div class="slider">
                 <div class="slides-container">
-                    <img src="images/slide/bg-slide.jpg" alt="" />
-                    <img src="images/slide/bg-slide.jpg" alt="" />
+                    <img src="images/cover.jpg" alt="" />
+                    <img src="images/cover.jpg" alt="" />
                 </div>
             </div>
             <div class="tint">
@@ -55,58 +35,33 @@
                     </div>
                     <div class="col-lg-12 col-lg-offset-1 col-md-12 col-sm-12 col-xs-12">
                         <h2 style="font-family: PT Sans, sans-serif; color: black;">LUMiNANCE</h2>
-<!--                        <img src="images/logo-teal.png" class="col-lg-3 col-md-6 col-sm-6 col-xs-12"/>-->
+<!--                        <img src="images/logo.png" class="col-lg-3 col-md-6 col-sm-6 col-xs-12"/>-->
                     </div>
-                    <div class="col-lg-11 col-lg-offset-1 col-md-12 col-sm-12 col-xs-12" style="left: 5%;">
-<!--                        <a href="https://www.facebook.com/luminance.lk" target="_blank" class="btn btn-default btn-lg"><i class="fa fa-facebook fa-lg"></i></a>-->
-<!--                        <a href="https://www.instagram.com/avixproductions/" target="_blank" class="btn btn-default btn-lg"><i class="fa fa-instagram fa-lg"></i></a>-->
-<!--                        <a href="mailto:info@avix.lk" target="_blank" class="btn btn-default btn-lg"><i class="fa fa-envelope fa-lg"></i></a>-->
-                    </div>
+<!--                    <div class="col-lg-11 col-lg-offset-1 col-md-12 col-sm-12 col-xs-12" style="left: 5%;">-->
+<!--                        <a href="https://www.facebook.com/luminance.lk/" target="_blank" class="btn btn-default btn-lg"><i class="fab fa-facebook fa-lg"></i></a>-->
+<!--                        <a href="mailto:info@luminance.lk" target="_blank" class="btn btn-default btn-lg"><i class="fa fa-envelope fa-lg"></i></a>-->
+<!--                    </div>-->
                 </div>
             </div>
         </div>
-
-        <!-- ========== BANNER END ========== -->
-
-        <div id="home"></div>
-
-        <!-- ========== FOOTER START ========== -->
 
         <footer class="navbar-fixed-bottom">
             <div class="container">
                 <div class="row text-center">
                     <div class="col-sm-12">
                         <p>
-                            <span><a href="https://www.facebook.com/luminance.lk" target="_blank"><i class="fa fa-facebook fa-lg"></i> Luminance</a></span>
-<!--                            <span><a href="https://www.instagram.com/avixproductions/" target="_blank"><i class="fa fa-instagram fa-lg"></i> Luminance</a></span>-->
+                            <span><a href="https://www.facebook.com/luminance.lk/" target="_blank"><i class="fab fa-facebook fa-lg"></i> Luminance</a></span>
                             <span><i class="fa fa-phone fa-lg"></i><a href="tel:+94767632424"> +94 76 763 2424</a></span>
-<!--                            <span><i class="fa fa-envelope fa-lg"></i><a href="mailto:info@avix.lk"> info@avix.lk</a></span>-->
-                            <span><i class="fa fa-map-marker fa-lg"></i><a href="https://goo.gl/maps/ukPvzpBUR2N2" target="_blank"> Battaramulla, Colombo, Sri Lanka. 10120</a></span>
+                            <span><i class="fa fa-envelope fa-lg"></i><a href="mailto:info@luminance.lk"> info@luminance.lk</a></span>
+                            <span><i class="fa fa-map-marker fa-lg"></i><a> Battaramulla, Colombo, Sri Lanka. 10120</a></span>
                         </p>
                     </div>
                 </div>
             </div>
         </footer>
 
-        <!-- ========== FOOTER END ========== -->
-
-        <div class="modal fade" id="viewModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"></div>
-
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="js/jquery-1.10.2.min.js"></script>
-
-        <!-- Bootstrap Plugins -->
-        <script src="js/bootstrap.min.js"></script>
-
-        <!-- Retina Plugin -->
-        <script src="js/retina-1.1.0.min.js"></script>
-
-        <!-- Superslides Plugin -->
-        <script src="js/jquery.easing.1.3.js"></script>
-        <script src="js/jquery.animate-enhanced.min.js"></script>
         <script src="js/jquery.superslides.js"></script>
-
-        <!-- jQuery Settings -->
         <script src="js/settings.js"></script>
     </body>
 </html>
